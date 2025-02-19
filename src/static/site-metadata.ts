c4e12b43ab4e8fe868ec1dc5ced2a0aa2e83ca06
vtku <guanqun.zhang@outlook.com>
2025-02-19 05:00:17 +0000

    update

---
+++ src/static/site-metadata.ts
@@ -0,0 +1,29 @@
+interface ISiteMetadataResult {
+  siteTitle: string;
+  siteUrl: string;
+  description: string;
+  logo: string;
+  navLinks: {
+    name: string;
+    url: string;
+  }[];
+}
+
+const data: ISiteMetadataResult = {
+  siteTitle: 'Go Running',
+  siteUrl: 'https://www.baidu.com',
+  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  description: 'Personal site and blog',
+  navLinks: [
+    {
+      name: 'Blog',
+      url: 'https:www.baidu.com',
+    },
+    {
+      name: 'About',
+      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+    },
+  ],
+};
+
+export default data;
